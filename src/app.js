9b3af4929520cd65a5e21ec026732dacbe4c3951
TomAmos98 <tom.amos@medieinstitutet.se>
2026-09-14 12:50:11 +0200

    feat: implement GET goalkeeper by id

--- src/app.js
+++ src/app.js
@@ -8,4 +8,20 @@ app.get('/api/goalkeepers', (req, res) => {
   res.status(200).json([]);
 });
 
+app.get('/api/goalkeepers/:id', (req, res) => {
+  const goalkeeper = {
+    id: 1,
+    name: 'Tom Amos',
+    club: 'Arsenal',
+    nationality: 'Sweden',
+    age: 28,
+    league: 'Premier League',
+    appearances: 25,
+    cleanSheets: 12,
+    savePercentage: 78
+  };
+
+  res.status(200).json(goalkeeper);
+});
+
 module.exports = app;
